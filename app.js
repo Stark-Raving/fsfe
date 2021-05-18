deb4e3194cbefa1ae14dd638980f2dde28d303a1
D <dave@Daves-MacBook-Pro.local>
2021-05-18 16:46:20 -0700

    added alt path

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.get("/", (req, res) => {
 	res.send("hello world");
 });
+app.get("/wha", (req, res) => {
+	res.status(418);
+	res.set("X-Wow", "Yep");
+	res.send("Weird!");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
